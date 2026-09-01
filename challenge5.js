7277bfe360b60db21f315dc21e4446d97f77970b
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-01 14:58:35 +0100

    5th challenge

---
+++ challenge5.js
@@ -0,0 +1,19 @@
+var prompt = require('prompt-sync')();
+
+var n = prompt('fournit la durée du film en minutes : ');
+let Duree = Number(n)
+if (Duree < 60) {
+    console.log(`Durée : ${Duree} \n`);
+    console.log(`Catégorie : Court métrage`);
+
+}
+else if (Duree > 120) {
+    console.log(`Durée: ${Duree} \n`);
+    console.log(`Catégorie: Film long`);
+}
+
+else {
+    console.log(`Durée: ${Duree} \n`);
+    console.log(`Catégorie: Film standard`);
+}
+process.exit(0);
