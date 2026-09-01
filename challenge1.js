280f24798a04344e41f9c998365159c461978391
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-01 13:14:13 +0100

    first challenge

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+{let Pseudo = 'ShadowX';
+let Niveau = 12;
+let Age = 21;
+let Score =4580;
+let Pays = 'Maroc';
+console.log(Pseudo,'',Niveau,'',Age,'',Score,'',Pays);}
+
+
+
+{let MAD = 1 ;
+MAD = 2200;
+let EUR = MAD/11;
+console.log('BUdget en MAD :',MAD);
+console.log('BUdget en EUR :',EUR);
+}
