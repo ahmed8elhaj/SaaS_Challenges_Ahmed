2891eb3070c511212421aeb6acf9a6ebb4b71fea
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-04 08:55:31 +0100

    8th challenge of functions

---
+++ Fonctions/challenge8.js
@@ -0,0 +1,18 @@
+var prompt = require('prompt-sync')();
+let montant = +prompt("Entrer le montant : ");
+let taux = +prompt("Entrer le taux : ");
+
+let resultat = convertirMontant(montant, taux, formatMAD);
+console.log(resultat);
+
+function formatMAD(val) {
+    val = val + " DH";
+    return val
+}
+
+function convertirMontant(montant, taux, formatMAD) {
+    let multiplier = montant * taux;
+    return formatMAD(multiplier)
+}
+
+
