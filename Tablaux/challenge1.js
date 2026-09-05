cbe00fa95970496849fec0f29adbd2d8acd39957
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-05 09:29:21 +0100

    1st challenge af arrays

---
+++ Tablaux/challenge1.js
@@ -0,0 +1,11 @@
+const element = [10, 20, 30, 40, 50];
+
+function afficherElements(tab) {
+    tab = element.length;
+
+    for (i = 0; i < tab; i++) {
+        console.log(element[i]);
+    }
+    return
+}
+afficherElements();
