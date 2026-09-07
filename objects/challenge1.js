1c037b3c1b17d89114c1dfe04820d656543b3a55
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-07 10:19:30 +0100

    2nd challenge of objects

---
+++ objects/challenge1.js
@@ -0,0 +1,8 @@
+let student = {
+    firstName: "Yassine",
+    lastName: "Amrani",
+    age: 21,
+    city: "Youssoufia",
+    school: "Youcode"
+}
+console.log(`${student.firstName} ${student.lastName} a ${student.age} ans et habite à ${student.city}.`);
---
+++ objects/challenge2.js
@@ -0,0 +1,12 @@
+let user = {
+    username: "Ahmed",
+    age: 22,
+    city: "Marrakech",
+    status: "student"
+};
+console.log(user.age);
+user.age=30;
+user.city="Youssoufia";
+user.status="prof"
+user.email="ahmed@exemple.com"
+console.log(user);
