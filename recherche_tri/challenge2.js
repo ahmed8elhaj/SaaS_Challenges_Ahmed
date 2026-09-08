b832a147d45e35bd988a127ca30ad6b286684755
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-08 14:45:09 +0100

    2nd challenge using loop instead of .sort()

--- recherche_tri/challenge2.js
+++ recherche_tri/challenge2.js
@@ -1,3 +1,11 @@
 let notes = [12, 5, 19, 3, 27, 8, 14];
-notes.sort((a,b)=>a-b);
+for(i=0;i<notes.length;i++){
+    for(j=0;j<notes.length-1-i;j++){
+        if(notes[j]>notes[j+1]){
+            let temp =notes[j];
+            notes[j]=notes[j+1]
+            notes[j+1]=temp
+        }
+    }
+}
 console.log(notes);
