d6a81ffb2c11948511cdfcece0a58a2926805ede
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-08 15:09:35 +0100

    3th challenge of recherche_tri

--- recherche_tri/challenge3.js
+++ recherche_tri/challenge3.js
@@ -1,5 +1,20 @@
 let ids = [2, 7, 11, 15, 23, 34, 45, 58, 61, 70];
 function findNumbre(nombre) {
-
-    ids.indexOf(nombre)
+    let start = 0;
+    let end = ids.length - 1;
+    while (start <= end) {
+        let middle = (start + end) >> 1;
+        if (ids[middle] === nombre) {
+            console.log(true, ' index : ', middle);
+            return
+        }
+        if (ids[middle] < nombre) {
+            start = middle + 1;
+        }
+        else {
+            end = middle - 1
+        }
+    }
+    console.log(false);
 }
+findNumbre(11)
