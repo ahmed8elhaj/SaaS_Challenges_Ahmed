0ad6eb5486a1b92f86f34ccf3e6a4a49a4cf6a5b
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-08 11:39:07 +0100

    the code of challenge 10, we add functions and loop do-while

--- objects/challenge10.js
+++ objects/challenge10.js
@@ -1,18 +1,5 @@
 var prompt = require('prompt-sync')();
-console.log(`
-=============================
-     STUDENT MANAGER
-=============================`);
-
-console.log("1. Afficher tous les étudiants");
-console.log("2. Rechercher un étudiant");
-console.log("3. Rechercher par ville");
-console.log("4. Afficher les étudiants admis");
-console.log("5. Afficher les étudiants non admis");
-console.log("6. Afficher le meilleur étudiant");
-console.log("7. Afficher la moyenne de la classe");
-console.log("8. Quitter");
-var choix = +prompt("Votre choix :");
+let choix;
 let students = [
     {
         id: 1,
@@ -50,68 +37,45 @@ let students = [
         note: 9
     }
 ];
+do
+{console.log(`
+=============================
+     STUDENT MANAGER
+=============================`);
+
+console.log("1. Afficher tous les étudiants");
+console.log("2. Rechercher un étudiant");
+console.log("3. Rechercher par ville");
+console.log("4. Afficher les étudiants admis");
+console.log("5. Afficher les étudiants non admis");
+console.log("6. Afficher le meilleur étudiant");
+console.log("7. Afficher la moyenne de la classe");
+console.log("8. Quitter");
+ choix = +prompt("Votre choix :");
+
 switch (choix) {
     case 1:
-        console.log(students);
+        afficherEtudiant(students)
         break;
     case 2:
-        let studentToFind = prompt("Entrer le nom :");
-        let studentFound = false;
-        for (i = 0; i < students.length; i++) {
-            if (students[i].name == studentToFind) {
-                console.log(students[i]);
-                studentFound = true;
-            }
-        }
-        if (!studentFound) {
-            console.log('not found');
-        }
+        let id = prompt("Entrer l'id  :");
+        rechercherEtudiant(id)
         break;
     case 3:
-        let cityToFind = prompt("Entrer la ville :");
-        let cityFound = false;
-        for (i = 0; i < students.length; i++) {
-            if (students[i].city == cityToFind) {
-                console.log(students[i]);
-                cityFound = true;
-            }
-        }
-        if (!cityFound) {
-            console.log('not found');
-        }
+        let city = prompt("Entrer la ville :");
+        findCity(city)
         break;
     case 4:
-        for (i = 0; i < students.length; i++) {
-            if (students[i].note >= 10) {
-                console.log(students[i]);
-            }
-        }
+        noteAdmis()
         break;
     case 5:
-        for (i = 0; i < students.length; i++) {
-            if (students[i].note <= 10) {
-                console.log(students[i]);
-            }
-        }
+        noteNotAdmis()
         break;
     case 6:
-        let noteMax = students[0].note;
-        for (i = 1; i < students.length; i++) {
-            if (noteMax < students[i].note) {
-                noteMax = students[i].note
-            }
-        }
-        let studentMaxNote = students.find(maxNote => maxNote.note == noteMax)
-        console.log(studentMaxNote);
+        noteMax()
         break;
     case 7:
-        let noteTotal = 0;
-        let notAvrg
-        for (i = 0; i < students.length; i++) {
-            noteTotal = noteTotal + students[i].note
-        }
-        notAvrg = noteTotal / (students.length);
-        console.log(`la moyenne de la classe : ${notAvrg}`);
+        calculeNoteMoyenne()
         break;
     default:
         console.log('Mercie pour votre viste. ');
@@ -119,4 +83,65 @@ switch (choix) {
         break;
 
 
+}}while (choix !== 8);
+function afficherEtudiant(student) {
+    console.table(student);
+}
+function rechercherEtudiant(studentToFind) {
+    let studentFound = false;
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].id == studentToFind) {
+            console.log(students[i]);
+            studentFound = true;
+        }
+    }
+    if (!studentFound) {
+        console.log('not found');
+    }
+}
+function findCity(cityToFind) {
+    let cityFound = false;
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].city == cityToFind) {
+            console.log(students[i]);
+            cityFound = true;
+        }
+    }
+    if (!cityFound) {
+        console.log('not found');
+    }
+
+}
+function noteAdmis() {
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].note >= 10) {
+            console.log(students[i]);
+        }
+    }
+}
+function noteNotAdmis() {
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].note <= 10) {
+            console.log(students[i]);
+        }
+    }
+}
+function noteMax() {
+    let noteMax = students[0].note;
+    for (let i = 1; i < students.length; i++) {
+        if (noteMax < students[i].note) {
+            noteMax = students[i].note
+        }
+    }
+    let studentMaxNote = students.find(maxNote => maxNote.note == noteMax)
+    console.log(studentMaxNote);
+}
+function calculeNoteMoyenne() {
+    let noteTotal = 0;
+    let notAvrg
+    for (let i = 0; i < students.length; i++) {
+        noteTotal = noteTotal + students[i].note
+    }
+    notAvrg = noteTotal / (students.length);
+    console.log(`la moyenne de la classe : ${notAvrg}`);
 }
