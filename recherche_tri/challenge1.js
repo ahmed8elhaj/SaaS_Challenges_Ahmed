43d174790648ea4ba4dba9fb76ca7c64af20e176
ahmed8elhaj <ahmed8elhaj@gmail.com>
2026-09-08 14:30:16 +0100

    the 1st challenge without indexOf

--- recherche_tri/challenge1.js
+++ recherche_tri/challenge1.js
@@ -1,11 +1,14 @@
 let nombres = [4, 8, 15, 16, 23, 42];
-function rechercheNombre(nombre){
-  if( nombres.indexOf(nombre)>0){
-    console.log(true);
-  }
-  else{
+
+function rechercheNombre(nombre) {
+    for (let i = 0; i < nombres.length; i++) {
+        if (nombres[i] === nombre) {
+            console.log(true,'index :', i);
+            return;
+        }
+    }
+
     console.log(false);
-    
-  }
 }
-rechercheNombre(8)
+
+rechercheNombre(42);
---
+++ recherche_tri/challenge3.js
@@ -0,0 +1,5 @@
+let ids = [2, 7, 11, 15, 23, 34, 45, 58, 61, 70];
+function findNumbre(nombre) {
+
+    ids.indexOf(nombre)
+}
